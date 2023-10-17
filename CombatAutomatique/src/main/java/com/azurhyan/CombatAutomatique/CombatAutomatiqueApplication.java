211dc64cf8029bdf2d50a39f43714b1ebe0d3d94
GarlandNic <nicolas-garland@club-internet.fr>
2023-10-17 23:51:16 +0200

    first

---
+++ CombatAutomatique/src/main/java/com/azurhyan/CombatAutomatique/CombatAutomatiqueApplication.java
@@ -0,0 +1,13 @@
+package com.azurhyan.CombatAutomatique;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CombatAutomatiqueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CombatAutomatiqueApplication.class, args);
+	}
+
+}
---
+++ CombatAutomatique/src/test/java/com/azurhyan/CombatAutomatique/CombatAutomatiqueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.azurhyan.CombatAutomatique;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CombatAutomatiqueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
